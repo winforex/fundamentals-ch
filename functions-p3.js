4c086230e3f58810dfe042ef450c6cb85e0f9465
Winf <winforex2@gmail.com>
2018-05-17 22:33:41 -0400

    test and functions-p3

---
+++ functions-p3.js
@@ -0,0 +1,16 @@
+function add7(number){
+    return number + 7
+}
+
+function multiply(num1, num2){
+    return num1 * num2
+}
+
+function capitalize(string){
+    let final = string[0].toUpperCase() + string.slice(1, string.length)
+    return final;
+}
+
+function lastLetter(string){
+    return string.slice(-1)
+}
